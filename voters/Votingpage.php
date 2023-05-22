e5d30bff1714944469f7eeff3f71f69508fb61ba
Abhishek Bohora <abhishek.bohora16@gmail.com>
2023-05-22 18:28:28 +0545

    fix: vote goes to correct candidate

--- voters/Votingpage.php
+++ voters/Votingpage.php
@@ -1,5 +1,5 @@
 <?php
-require_once("include/header.php");
+require_once ("include/header.php");
 $id = $_GET['Election_id'];
 $sql = "SELECT * FROM `elections` WHERE id = '$id' LIMIT 1";
 $result = mysqli_query($con, $sql);
@@ -18,88 +18,85 @@ $Election_id = $row['id'];
     <table class="table ">
     <thead>
         <tr class="bg_green ">
-            <th colspan="4" class="text-white"><h4> ELECTION TOPIC: <?php echo strtoupper($Name);  ?> <h4></th>
+            <th colspan="4" class="text-white"><h4> ELECTION TOPIC: <?php echo strtoupper($Name); ?> <h4></th>
         </tr>
         <tr>
             <th> Candidate Photo</th>
             <th> Candidate Details</th>
-            <!-- <th> Votes</th> -->
+            <th> Votes</th>
             <th> Action</th>
 
         </tr>
 </thead>
 <tbody>
         <?php
-            $FetchingCandidates = mysqli_query($con, "SELECT * FROM candidate_details WHERE election_id = '". $Election_id."'") or die(mysqli_error($con));
-        
-        //print_r($FetchingCandidates);
-        while($candidateData = mysqli_fetch_assoc($FetchingCandidates))
-        {
-            $candidate_id = $candidateData['id'];
-            $candidate_Photo = $candidateData['Candidate_Photo'];
+$FetchingCandidates = mysqli_query($con, "SELECT * FROM candidate_details WHERE election_id = '" . $Election_id . "'") or die(mysqli_error($con));
 
-            // Fetching Candidate Votes.
-            $FetchingVotes = mysqli_query($con, "SELECT * FROM votings WHERE candidate_id = '". $candidate_id ."'") or die(mysqli_error($con));
-            $TotalVotes = mysqli_num_rows($FetchingVotes);
-		
-        //echo $_SESSION['user_id'];
-            ?>
+//print_r($FetchingCandidates);
+while ($candidateData = mysqli_fetch_assoc($FetchingCandidates))
+{
+    $candidate_id = $candidateData['id'];
+    $candidate_Photo = $candidateData['Candidate_Photo'];
+
+    // Fetching Candidate Votes.
+    $FetchingVotes = mysqli_query($con, "SELECT * FROM votings WHERE candidate_id = '" . $candidate_id . "'") or die(mysqli_error($con));
+    $TotalVotes = mysqli_num_rows($FetchingVotes);
+
+    //echo $_SESSION['user_id'];
+    
+?>
                 <tr>
                     <td><img src="<?php echo $candidate_Photo ?> " class="Candidate_photo"></td>
-                    <td><?php echo "<b>".$candidateData['Candidate_Name'] ."</b><br />" .$candidateData['Candidate_Details'];  ?></td>
-                    <!-- <td><?php 
-                    // echo $TotalVotes;
-                     ?> 
-                     </td> -->
+                    <td><?php echo "<b>" . $candidateData['Candidate_Name'] . "</b><br />" . $candidateData['Candidate_Details']; ?></td>
+                    <td><?php echo $TotalVotes; ?> </td>
                     
                      <td>
                         <?php
-                        $CheckIfVoteCasted = mysqli_query($con, "SELECT * FROM votings WHERE voters_id = '". $_SESSION['user_id'] ."' AND election_id = '". $Election_id."'")
-                         or die(mysqli_error($con));
-                         $IsVoteCasted = mysqli_num_rows($CheckIfVoteCasted);
+    $CheckIfVoteCasted = mysqli_query($con, "SELECT * FROM votings WHERE voters_id = '" . $_SESSION['user_id'] . "' AND election_id = '" . $Election_id . "'") or die(mysqli_error($con));
+    $IsVoteCasted = mysqli_num_rows($CheckIfVoteCasted);
 
-                        if($IsVoteCasted > 0)
-                        {
-                            $voteCastedData = mysqli_fetch_assoc($CheckIfVoteCasted);
-                            $voteCastedToCandidate = $voteCastedData['candidate_id'];
+    if ($IsVoteCasted > 0)
+    {
+        $voteCastedData = mysqli_fetch_assoc($CheckIfVoteCasted);
+        $voteCastedToCandidate = $voteCastedData['candidate_id'];
 
-                            if($voteCastedToCandidate == $candidate_id)
-                            {
-                                ?>
+        if ($voteCastedToCandidate == $candidate_id)
+        {
+?>
                                 <img src="../assets/images/v.png"width="100px;">
                                 <?php
-                            }
-                          
+        }
 
-                        }else{
-                           
-                            ?>
+    }
+    else
+    {
+
+?>
 
                             
-                            <button class="btn btn-md btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal"> Vote </button> 
+                <button class="btn btn-md btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal-<?php echo $candidate_id; ?>"> Vote </button>
 
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Confirmation</h1>
-
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        Are You Sure You Want to Vote?
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
-        <button type="button" class="btn btn-primary"  onclick="castvote(<?php echo  $Election_id;?>,   <?php  echo $candidate_id;?>, <?php  echo $_SESSION['user_id'];?> )">Yes</button>
-      </div>
-    </div>
-  </div>
-</div>
+                <div class="modal fade" id="exampleModal-<?php echo $candidate_id; ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Confirmation</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Are You Sure You Want to Vote?
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                            <button type="button" class="btn btn-primary" onclick="castvote(<?php echo $Election_id; ?>, <?php echo $candidate_id; ?>, <?php echo $_SESSION['user_id']; ?>)">Yes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
                             <?php
-                        }
+    }
 
 ?>
                           <!-- <button class="btn btn-md btn-success" onclick="castvote()"> Vote </button>  -->
@@ -109,8 +106,8 @@ $Election_id = $row['id'];
 
                 </tr>
             <?php
-        }
-        ?>
+}
+?>
      </tbody>
 </table>
 
@@ -126,7 +123,6 @@ $Election_id = $row['id'];
 
     const castvote = (election_id, candidate_id, voters_id) =>
     {
-       //console.log(election_id + "-" + candidate_id + "-" +   voters_id);
       $.ajax({
        type: "POST",
        url: "include/Ajaxcalls.php",
@@ -150,5 +146,5 @@ $Election_id = $row['id'];
 
 
 <?php
-require_once("include/footer.php");
+require_once ("include/footer.php");
 ?>
